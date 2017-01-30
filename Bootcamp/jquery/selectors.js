9e362ee635c3ba4f34ef19ce7c39ae900c631166
Deborah Newberry <dnewberry16@gmail.com>
2017-01-30 12:09:41 -0800

    finished jquery intro

---
+++ Bootcamp/jquery/selectors.js
@@ -0,0 +1,11 @@
+/* all divs purple bkg */
+$("div").css("backgroundColor", "purple");
+
+/* all .highlight 200px wide */
+$(".highlight").css("width", "200px");
+
+/* #third orange border */
+$("#third").css("border", "2px solid orange");
+
+/* first div font color pink */
+$("div:first-of-type").css("color", "pink");
