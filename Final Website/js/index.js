d5f767b2a0d196fd6c09aa46809081a6c0f4b790
Deborah Newberry <dnewberry16@gmail.com>
2017-02-15 19:33:37 -0800

    update to final website

---
+++ Final Website/js/index.js
@@ -0,0 +1,9 @@
+// var link = document.getElementById("resume")
+
+// // link.href = "resume.pdf";
+// link.download = "resume.pdf";
+// link.dispatchEvent(new MouseEvent('click'));
+
+
+var button = document.getElementById("resume-btn");
+console.log(button)
